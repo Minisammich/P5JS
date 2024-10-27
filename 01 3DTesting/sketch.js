d254275994a8485862b24adeecdaab55e926cf80
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-10-27 17:55:20 -0600

    Getting somewhere
    Camera movement works a bit (still haven't properly implemented up and down) and on screen text now works.

--- 01 3DTesting/sketch.js
+++ 01 3DTesting/sketch.js
@@ -5,23 +5,92 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let Roboto;
+
+let cam;
+let xCam = 0;
+let yCam = 0;
+let zCam = 800;
+let lookX = 0;
+let lookY = 0;
+
+function preload() {
+  Roboto = loadFont("Roboto/Roboto-Regular.ttf");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
+  textFont(Roboto);
+  cam = createCamera();
+  cam.lookAt(0,0,0);
+  setCamera(cam);
+}
+
+function keyChecks() {
+  if(keyIsDown(188)) {
+    zCam -= 10;
+  } else if(keyIsDown(79)) {
+    zCam += 10;
+  } 
+
+  if(keyIsDown(65)) {
+    xCam -= 10;
+  } else if(keyIsDown(69)) {
+    xCam += 10;
+  }
+
+  if(keyIsDown(LEFT_ARROW)) {
+    lookX += 5;
+    if(lookX >= 360) lookX = 0;
+  } else if(keyIsDown(RIGHT_ARROW)) {
+    lookX -= 5;
+    if(lookX <= -360) lookX = 0;
+  }
+
+  
+  if(keyIsDown(UP_ARROW) && lookY > -85) {
+    lookY -= 5;
+  } else if(keyIsDown(DOWN_ARROW) && lookY < 85) {
+    lookY += 5;
+  }
+
+}
+
+function onScreenText(screenText,x,y) {
+  push();
+  translate(xCam,yCam,zCam);
+  rotateY(180+lookX);
+  rotateX(lookY);
+  translate(0,0,-120);
+  text(screenText,-width/13+x,-height/13+y);
+  pop();
 }
 
 function draw() {
-  push();
   background(220);
+  cube_orbiting_globe();
+  push();
+  translate(0,0,1000);
+  cube_orbiting_globe();
+  pop();
+  keyChecks();
+  cam.setPosition(xCam,yCam,zCam);
+  //cam.lookAt(xCam+360*sin(lookX),yCam+360*sin(lookY),zCam-20-360*cos(lookX)-360*cos(lookY));
+  cam.lookAt(xCam+sin(lookX),yCam+sin(lookY),zCam+1+(cos(lookX)-cos(lookY)));
+  onScreenText(lookX,10,15);
+  onScreenText(lookY,10,25);
+}
+
+function cube_orbiting_globe() {
+  push();
   rotateY(frameCount);
   fill(0,0,255);
   stroke(100,200,100);
-  sphere(50,24,24);
+  sphere(100,24,24);
   push();
   fill(100,100,150);
-  translate((width/2)-200,0,0);
-  rotateY(frameCount);
+  translate((width/2)-400,0,0);
   rotateY(0);
   rotateX(45);
   rotateZ(45);
